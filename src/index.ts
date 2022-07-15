d0a2285aa8d011b2d8be79f2b32e5c0bb2c995f0
Bùi Xuân Hiếu <66052667+bxhieu713@users.noreply.github.com>
2022-07-15 22:47:19 +0700

    thay doi o branch

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-let array: number[] = [1, 2, 3];
+let array: number[] = [1, 2, 3,5,7];
 
 let value = array[10]; // with `noUncheckedIndexedAccess` this has the type `number | undefined`
 
+console.log(typeof value);
 console.log(typeof value);
